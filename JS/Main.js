e9dd6e4601429d72042a81a77acb440252f10c88
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-27 18:51:40 +0330

    feat: develop show result in ui

--- JS/Main.js
+++ JS/Main.js
@@ -1,9 +1,19 @@
 let $ = document;
 let StartBox = $.querySelector('#StartBox');
-let main = $.querySelector('#main');
 let ResultKeyCode = $.querySelector('#ResultKeyCode');
 let KeyBody = $.querySelector('#KeyBody');
 let LocationBody = $.querySelector('#LocationBody');
 let CodeBody = $.querySelector('#CodeBody');
 let WhichBody = $.querySelector('#WhichBody');
 let DescriptionBody = $.querySelector('#DescriptionBody');
+
+$.body.addEventListener('keydown' , (event) => {
+    event.preventDefault();
+    StartBox.style.display = 'none';
+    ResultKeyCode.innerHTML = event.keyCode;
+    KeyBody.innerHTML = event.key;
+    LocationBody.innerHTML = event.location;
+    CodeBody.innerHTML = event.code;
+    WhichBody.innerHTML = event.which;
+    DescriptionBody.innerHTML = event.key;
+});
